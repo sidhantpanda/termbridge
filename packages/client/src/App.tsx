c86e9825137250861568317bea4dcae4af9f5ab6
Sidhant Panda <sidhantpanda@gmail.com>
2024-08-16 22:44:34 +0200

    Remove test route

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
-import AllTerminals from './pages/AllTerminals';
 import Terminal from './pages/Terminal';
 import './style.css';
 import AddRemote from './pages/AddRemote';
@@ -27,7 +26,6 @@ const App: React.FC = () => {
           <Routes>
             <Route path="/" element={<Layout />} >
               <Route path="/" element={<Home />} />
-              <Route path="/terminals" element={<AllTerminals />} />
               <Route path="/remotes/add" element={<AddRemote />} />
               <Route path="*" element={<h1>Not Found</h1>} />
               <Route path="/remotes/:id/edit" element={<EditRemote />} />
