38a13e04f7e9befb1118297ca14125bb8625095b
Sidhant Panda <sidhantpanda@gmail.com>
2024-08-24 11:15:53 +0200

    Add keyboard shortcuts

--- packages/client/src/pages/Terminal.tsx
+++ packages/client/src/pages/Terminal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TerminalContainer from '../components/Terminal/TerminalContainer';
 import { useNavigate, useParams } from 'react-router-dom';
 import LoggedOut from '../components/Terminal/LoggedOut';
@@ -8,7 +8,28 @@ const Terminal = () => {
   const { id_host } = useParams();
   const navigate = useNavigate();
   const [isLoggedOut, setIsLoggedOut] = React.useState(false);
-  const [id, ...rest] = id_host.split('-')
+  const [id, ...rest] = id_host.split('-');
+
+  const navigateToHome = () => {
+    navigate('/');
+  }
+
+  useEffect(() => {
+    if (isLoggedOut) {
+      const handleKeyPress = (e: KeyboardEvent) => {
+        if (e.ctrlKey) {
+          if (e.key === 't') {
+            setIsLoggedOut(false);
+          }
+          if (e.key === 'd') {
+            navigateToHome();
+          }
+        }
+      };
+      window.addEventListener('keydown', handleKeyPress);
+      return () => window.removeEventListener('keydown', handleKeyPress);
+    }
+  }, [isLoggedOut]);
 
   if (!isLoggedOut) {
     return (
@@ -24,7 +45,7 @@ const Terminal = () => {
     return (
       <Layout>
         <LoggedOut
-          onHomeRequested={() => navigate('/')}
+          onHomeRequested={navigateToHome}
           onReloadTerminalRequested={() => setIsLoggedOut(false)}
         />
       </Layout>
