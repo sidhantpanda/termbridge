7d74e03c0bc82f328020fc9abeb88bf60e437b1a
Sidhant Panda <sidhantpanda@gmail.com>
2025-04-22 11:18:30 +0200

    Cache tailscale info

--- packages/server/src/api/remotes/get-tailscale-info.ts
+++ packages/server/src/api/remotes/get-tailscale-info.ts
@@ -3,29 +3,36 @@ import { getRedisClient } from '../../lib/redis';
 import { getTailscaleInfo as getTSCommand } from '../../lib/tailscale';
 import RemoteHosts from '../../couchdb/RemoteHosts';
 
+const CACHE_TTL = 5 * 60; // 5 minutes
+
 const getTailscaleInfo: RequestHandler = async (req: Request, res) => {
   const _id = req.params.id;
-  // const remote = await RemoteHosts.get(remoteId);
-
   const client = await getRedisClient();
+
+  if (client) {
+    const cached = await client.get(`${_id}:tailscale-info`);
+    if (cached) {
+      // console.log('Using cached tailscale data for ', _id);
+      return res.send({ ips: JSON.parse(cached) });
+    }
+  }
+
   const existing = _id ? (await RemoteHosts.get(_id)) : undefined;
   if (_id && !existing) {
     res.status(404).send({ message: 'Remote not found' });
     return;
   }
+
   try {
-
     const ips = await getTSCommand(_id);
-
     if (client) {
-      // console.log('Caching containers data for ', remoteId);
-      // client.setEx(`${remoteId}:containers`, 30, JSON.stringify(containers));
+      console.log('Caching tailscale info for ', _id);
+      client.setEx(`${_id}:tailscale-info`, CACHE_TTL, JSON.stringify(ips));
     }
-    // const saved = await RemoteHosts.insert({ ...existing, _id, tailscaleIps: ips });
     return res.send({ ips });
   } catch (error) {
-    console.error('Error fetching containers:', error);
-    return res.status(500).send({ error: 'Error fetching containers' });
+    console.error('Error fetching tailscale info:', error);
+    return res.status(500).send({ error: 'Error fetching tailscale info' });
   }
 };
 
