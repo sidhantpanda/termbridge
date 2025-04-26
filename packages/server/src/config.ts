a69106626a0ee4dc716d207d692aafc1af86b3ba
Sidhant Panda <sidhantpanda@gmail.com>
2025-04-26 16:13:28 +0200

    Remove couch db configs

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -3,11 +3,6 @@ import path from 'path';
 export const IS_DEV = process.env.IS_DEV === '1';
 export const CLIENT_DIST = path.resolve(process.cwd(), '..', process.env.CLIENT_DIST ?? 'client/dist');
 
-export const COUCHDB_HOST = process.env.COUCHDB_HOST ?? 'localhost';
-export const COUCHDB_PORT = process.env.COUCHDB_PORT ?? '5984';
-export const COUCHDB_USER = process.env.COUCHDB_USER ?? 'admin';
-export const COUCHDB_PASS = process.env.COUCHDB_PASS ?? 'admin';
-
 export const PG_HOST = process.env.PG_HOST ?? 'localhost';
 export const PG_PORT = process.env.PG_PORT ?? '5432';
 export const PG_USER = process.env.PG_USER ?? 'postgres';
