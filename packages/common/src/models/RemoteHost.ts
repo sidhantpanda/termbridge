8d7f6388e45cc8a1e25b623588691fb2b60d78b4
Sidhant Panda <sidhantpanda@gmail.com>
2025-04-26 16:06:45 +0200

    Remove nano

--- packages/common/src/models/RemoteHost.ts
+++ packages/common/src/models/RemoteHost.ts
@@ -1,10 +1,9 @@
 import { ConnectConfig } from 'ssh2';
-import { DocumentGetResponse } from 'nano';
 
 type PartialConnectConfig = Omit<
   ConnectConfig,
   'debug' | 'hostVerifier' | 'password' | 'privateKey'
-> & DocumentGetResponse;
+>
 
 interface RemoteHost extends PartialConnectConfig {
   id: string;
